5c492c08b577ceee0a956c4ec52e0387f04d01cd
meccar <tung14hn@gmail.com>
2024-08-13 22:40:18 +0800

    created client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf6a7e22a441dc54d13a66f2437c3bd84db0e2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8edeac19758b45ffd332b28146855f7eaccfc838")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
