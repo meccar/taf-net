593ac1cf941f79586efdb6667e455fcd84d5bda0
meccar <tung14hn@gmail.com>
2024-08-15 22:20:04 +0800

    feat(client): updated app

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c492c08b577ceee0a956c4ec52e0387f04d01cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69401b94d56c316d6fa955f7147678942d77815")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
