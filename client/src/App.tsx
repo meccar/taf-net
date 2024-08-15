0992c763f5dc62df75d2d143851d02891277aa60
meccar <tung14hn@gmail.com>
2024-08-15 22:33:33 +0800

    feat(client): updated app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
 
+const products = [
+  { name: "product1", price: 10 },
+  { name: "product2", price: 20 },
+];
+
 function App() {
   return (
     <div>
-      <h1>Taf</h1>
+      <div>
+        <h1>Taf</h1>
+      </div>
+      <ul>
+        {products.map((item) => (
+          <li key={item.name}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
